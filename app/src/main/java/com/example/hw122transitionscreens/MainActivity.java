b579c4feff92990621568819006d35ac32e41fb9
Dmitriy25 <ohapkin_da@mail.ru>
2020-01-17 09:12:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw122transitionscreens/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hw122transitionscreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        Button btnForward = findViewById(R.id.btnForward);
+        Button btnBack = findViewById(R.id.btnBack);
+        TextView link = findViewById(R.id.link);
+
+        link.setText("http://myfile.org/" + new Random().nextInt(100) + 1);
+
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
